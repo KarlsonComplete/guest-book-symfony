f33f70a62c90cab746d9c9d6d89fa60c7eab12a3
Кирилл Храмов <hramovkirill01@gmail.com>
2022-10-03 13:45:33 +0300

    - Добавлен постраничный вывод комментариев

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 use App\Repository\ConferenceRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class ConferenceController extends AbstractController
 {
@@ -20,11 +21,16 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{id}', name: 'conference')]
-    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
     {
+        $offset = max(0,$request->query->getInt('offset',0));
+        $paginator = $commentRepository->getCommentPaginator($conference,$offset);
+
         return new Response($twig->render('conference/show.html.twig',[
             'conference' => $conference,
-            'comments' => $commentRepository->findBy(['conference'=>$conference],['createdAt'=>'DESC']),
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator),$offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]));
     }
 }
