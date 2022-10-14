803e1bcb2575ba54a1146ffe8cb28c23be4a2c7e
Кирилл Храмов <hramovkirill01@gmail.com>
2022-10-14 10:56:38 +0300

    - теперь мы определяем окружение twig не в аргументе метода, а в конструкторе.

--- migrations/Version20221014065451.php
+++ migrations/Version20221014065451.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220927093939 extends AbstractMigration
+final class Version20221014065451 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,17 +20,12 @@ final class Version20220927093939 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment ADD conference_id INT NOT NULL');
-        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C604B8382 FOREIGN KEY (conference_id) REFERENCES conference (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('CREATE INDEX IDX_9474526C604B8382 ON comment (conference_id)');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE comment DROP CONSTRAINT FK_9474526C604B8382');
-        $this->addSql('DROP INDEX IDX_9474526C604B8382');
-        $this->addSql('ALTER TABLE comment DROP conference_id');
     }
 }
