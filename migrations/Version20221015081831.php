aa4954bbb568aacb7c018dd21fcb6fca2b84f272
Кирилл Храмов <hramovkirill01@gmail.com>
2022-10-15 11:35:27 +0300

    -fix error (all work)

--- migrations/Version20221015081831.php
+++ migrations/Version20221015081831.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221015070138 extends AbstractMigration
+final class Version20221015081831 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,13 @@ final class Version20221015070138 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE sessions (
-            sees_id VARCHAR (128) NOT NULL PRIMARY KEY,
-            sess_data BYTEA NOT NULL,
-            sess_lifetime INTEGER NOT NULL,
-            sess_time INTEGER NOT NULL
-            )
+        $this->addSql('
+         CREATE TABLE sessions (
+         sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
+         sess_data BYTEA NOT NULL,
+         sess_lifetime INTEGER NOT NULL,
+         sess_time INTEGER NOT NULL
+         )
             ');
     }
 
