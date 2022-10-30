313cfd9a4e4ea7099c5e37b04956da3214422124
Кирилл Храмов <hramovkirill01@gmail.com>
2022-10-30 14:43:50 +0300

    -add feedback form

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
+use App\Form\CommentFormType;
+use App\Entity\Comment;
 use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,6 +33,8 @@ class ConferenceController extends AbstractController
     public function show(Request $request, Conference $conference, CommentRepository $commentRepository):Response
     {
 
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class,$comment);
         $offset = max(0,$request->query->getInt('offset',0));
         $paginator = $commentRepository->getCommentPaginator($conference,$offset);
         return new Response($this->twig->render('conference/show.html.twig',[
@@ -38,6 +42,7 @@ class ConferenceController extends AbstractController
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator),$offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView(),
         ]));
     }
 }
