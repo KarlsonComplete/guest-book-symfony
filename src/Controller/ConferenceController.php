061e6d5f49cb8dc81f46e19b550d752d5df1eef3
Кирилл Храмов <hramovkirill01@gmail.com>
2022-11-26 12:22:59 +0300

    - added protection spam with-api(how to check the work?)

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use App\SpamChecker;
 use App\Repository\ConferenceRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -28,44 +29,54 @@ class ConferenceController extends AbstractController
 
 
     #[Route('/', name: 'homepage')]
-    public function index(ConferenceRepository $conferenceRepository):Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
         return new Response($this->twig->render('conference/index.html.twig', ['conferences' => $conferenceRepository->findAll(),]));
     }
 
 
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir):Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir, SpamChecker $spamChecker): Response
     {
 
         $comment = new Comment();
-        $form = $this->createForm(CommentFormType::class,$comment);
+        $form = $this->createForm(CommentFormType::class, $comment);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $comment->setConference($conference);
 
-            if ($photo = $form['photo']->getData()){
-                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
                 try {
-                    $photo->move($photoDir,$filename);
-                } catch (FileException $e){
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
                     // unable to upload the photo, give up
                 }
                 $comment->setPhotoFilename($filename);
             }
 
             $this->entityManager->persist($comment);
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException('Blatant spam, go away!');
+            }
             $this->entityManager->flush();
 
-            return $this->redirectToRoute('conference',['slug' => $conference->getSlug()]);
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
         }
-        $offset = max(0,$request->query->getInt('offset',0));
-        $paginator = $commentRepository->getCommentPaginator($conference,$offset);
-        return new Response($this->twig->render('conference/show.html.twig',[
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+        return new Response($this->twig->render('conference/show.html.twig', [
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-            'next' => min(count($paginator),$offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
             'comment_form' => $form->createView(),
         ]));
     }
